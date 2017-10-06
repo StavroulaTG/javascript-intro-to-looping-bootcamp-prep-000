ad064e8ca9de2987ff813f18790dae92b4d8ce91
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-06 02:02:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,12 +16,6 @@ function doWhileLoop(array) {
     array.pop();
   }
 while (array.length > 0 && maybeTrue());
-return array
+return array;
 }
-function doWhileLoop(array) {
-  do {
-    array.pop();
-  }
-  while (array.length > 0 && maybeTrue());
-  return array;
-}
+
